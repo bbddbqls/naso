63d9ee61eca74ed41ec3ad284323ddb86b9da854
bbddbqls <126733044+bbddbqls@users.noreply.github.com>
2023-09-04 17:44:25 +0900

    Delete naso directory

--- naso/src/main/webapp/js/fileupload.js
+++
@@ -1,7 +0,0 @@
-// index.js
-
-const button = document.getElementById('upload-button');
-button.addEventListener('click', () => {
-  const input = document.getElementById('input-photo');
-  putFile(input.files[0]);
-})
--- naso/src/main/webapp/js/login.js
+++
@@ -1,8 +0,0 @@
-// 로그인 정보 저장 스크립트
-function saveLoginInfo() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    localStorage.setItem('username', username);
-    localStorage.setItem('password', password);
-    alert('로그인 정보가 저장되었습니다.');
-  }
